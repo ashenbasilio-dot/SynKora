5ffbdc0de86a4f7a3a583dca80d360e15241c8a9
tyrustobbylizarondo <lizarondotyrustobby@gmail.com>
2026-09-18 18:08:06 +0800

    Tyrus Tobby Lizarondo mini peta

---
+++ app/src/test/java/com/example/synkora/quarter2/LizarondoTest.java
@@ -0,0 +1,22 @@
+package com.example.synkora.quarter2;
+
+import org.junit.Test;
+public class LizarondoTest {
+    @Test
+    public void myinfomation() {
+
+        String name = "Tyrus Lizarondo";
+        int age = 16;
+        String hobby = "Basketball";
+        String favoriteFood = "Pork Katsudon";
+        String goal = "My financial goal is to save money regularly for my future needs.\n";
+
+        System.out.println("===== ABOUT ME =====");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Hobby: " + hobby);
+        System.out.println("Favorite Food: " + favoriteFood);
+        System.out.println("My Goal: " + goal);
+        System.out.println("====================");
+    }
+}
