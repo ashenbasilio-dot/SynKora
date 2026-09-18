d3d4761264550829f88d25220612b44902151b7c
Student-Saoc-Tang <kyuukojie@gmail.com>
2026-09-18 21:55:59 +0800

    Tang mini peta

---
+++ app/src/test/java/com/example/synkora/quarter2/TangTest.java
@@ -0,0 +1,21 @@
+package com.example.synkora.quarter2;
+import org.junit.Test;
+public class TangTest {
+    @Test
+    public void main(){
+
+        String name = "Adriane Tang";
+        int age = 16;
+        String hobby = "Playing P5R";
+        String favoriteFood = "Pares";
+        String goal = "My financial goal is to save consistently, build an emergency fund, and grow my income so I can achieve long-term financial freedom.\n";
+
+        System.out.println("===== ABOUT ME =====");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Hobby: " + hobby);
+        System.out.println("Favorite Food: " + favoriteFood);
+        System.out.println("My Goal: " + goal);
+        System.out.println("====================");
+    }
+}
